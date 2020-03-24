6b068465cc12745850cfa0ae1af43ffd1b21142f
shubham5526 <41041596+shubham5526@users.noreply.github.com>
2020-03-24 20:00:03 +0530

    For Saber
    For Saber

--- api.py
+++ api.py
@@ -1,21 +1,88 @@
-#Import necessary packages
-from flask import Flask
-from flask_restful import Resource, reqparse, Api 
-#Instantiate a flask object 
-app = Flask(__name__)
-#Instantiate Api object
-api = Api(app)
-#Setting the location for the sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
-#Adding the configurations for the database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+import flask
+from flask import request, jsonify
+import requests
 
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
+API_ENDPOINT = "https://api-crt.cert.havail.sabre.com/v1/offers/shop"
+
+API_KEY = "T1RLAQKdwHQFmCgiuM/R7mCaG+6DXV9TYBBHHar0enUCDHVn6zBrxvn6AADAM7CWdWstzKO0VXPYRxhOAtGKWudnKSmGO0GwbWbc5wA/yg0lkHu6daO9cV6Af5Zhjk4jjH+7Som1Ycsukkr3gIF41qQEfnfE8eV+ee2O4KzXz1nvdOwZrcb6NXmWuH37f6c1G915GfVqVL1hj2ATIU4lXIi8QLkqkRO7nFcjZQbPFL2p0r7OitP+CsoQcqCJIHZC8sj350N8wmGxr60r0kX+epEWvJ97VkXVX+8Fu0n6FS9KbnRSN4/pmVJq9+2/"
+hed = {'Authorization': 'Bearer ' + API_KEY}
+data = {
+    "OTA_AirLowFareSearchRQ": {
+        "OriginDestinationInformation": [
+            {
+                "DepartureDateTime": "2020-06-21T00:00:00",
+                "DestinationLocation": {
+                    "LocationCode": "SFO"
+                },
+                "OriginLocation": {
+                    "LocationCode": "NYC"
+                },
+                "RPH": "0"
+            },
+            {
+                "DepartureDateTime": "2020-06-22T00:00:00",
+                "DestinationLocation": {
+                    "LocationCode": "NYC"
+                },
+                "OriginLocation": {
+                    "LocationCode": "SFO"
+                },
+                "RPH": "1"
+            }
+        ],
+        "POS": {
+            "Source": [
+                {
+                    "PseudoCityCode": "F9CE",
+                    "RequestorID": {
+                        "CompanyName": {
+                            "Code": "TN"
+                        },
+                        "ID": "1",
+                        "Type": "1"
+                    }
+                }
+            ]
+        },
+        "TPA_Extensions": {
+            "IntelliSellTransaction": {
+                "RequestType": {
+                    "Name": "200ITINS"
+                }
+            }
+        },
+        "TravelPreferences": {
+            "TPA_Extensions": {
+                "DataSources": {
+                    "ATPCO": "Enable",
+                    "LCC": "Disable",
+                    "NDC": "Disable"
+                },
+                "NumTrips": {}
+            }
+        },
+        "TravelerInfoSummary": {
+            "AirTravelerAvail": [
+                {
+                    "PassengerTypeQuantity": [
+                        {
+                            "Code": "ADT",
+                            "Quantity": 1
+                        }
+                    ]
+                }
+            ],
+            "SeatsRequested": [
+                1
+            ]
+        },
+        "Version": "1"
+    }
+}
+
 books = [
     {'id': 0,
      'title': 'A Fire Upon the Deep',
@@ -37,13 +104,12 @@ books = [
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/api/v1/resources/bargainer/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    r = requests.post(url=API_ENDPOINT, json=data, headers=hed)
+    return str(r.content)
 
 app.run()
